1540b3d5a749ff0170eea0e19e3dc376b988a52f
Yasser Hussain <yasserhussain1110@gmail.com>
2017-01-03 13:59:32 +0530

    Poll UI mostly done

---
+++ public/script.js
@@ -0,0 +1,82 @@
+function random(lower_limit, upper_limit) {
+  return ((Math.random() * (upper_limit - lower_limit + 1)) | 0) + lower_limit;
+}
+
+function onPollReady(answers) {
+  var ansItems = answers.map(function (ans) {
+    return ans.value;
+  });
+
+  var ansVote = answers.map(function (ans) {
+    return ans.vote;
+  });
+
+  buildOptions(ansItems);
+  buildDoughnut(ansItems, generateRandomColors(answers.length), ansVote)
+}
+
+function buildOptions(options) {
+  $("#user-choice").append(
+    options.map(function (optionVal) {
+      return $("<option/>").val(optionVal).html(optionVal);
+    }));
+}
+
+function padThree(num) {
+  num = "" + num;
+  return '0'.repeat(3 - num.length) + num;
+}
+
+function generateRandomColorNumbers(length) {
+  var randomColors = [];
+  for (var i = 0; i < length; i++) {
+
+    var a = padThree(random(0, 255));
+    var b = padThree(random(0, 255));
+    var c = padThree(random(0, 255));
+    var randomColor = a + b + c;
+
+    while (randomColors.indexOf(randomColor) !== -1) {
+      a = padThree(random(0, 255));
+      b = padThree(random(0, 255));
+      c = padThree(random(0, 255));
+      randomColor = a + b + c;
+    }
+    randomColors.push(randomColor);
+  }
+  return randomColors;
+}
+
+function generateRandomColors(length) {
+  var colorNumbers = generateRandomColorNumbers(length);
+  return colorNumbers.map(function (color) {
+    return 'rgba(' + color[0] + color[1] + color[2]
+      + ", " + color[3] + color[4] + color[5]
+      + ", " + color[6] + color[7] + color[8]
+      + ", 0.2)";
+  });
+}
+
+function buildDoughnut(labels, colors, data) {
+  var ctx = document.getElementById("voteChart").getContext("2d");
+  var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: '# of Votes',
+        data: data,
+        backgroundColor: colors,
+        borderWidth: 0
+      }]
+    },
+    options: {
+      cutoutPercentage: 30,
+      fullWidth: false,
+      legend: {
+        display: true,
+        position: 'bottom'
+      }
+    }
+  });
+}
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
+var dbUrl = process.env.DATABASE_URL || "mongodb://localhost:27017/voto";
 
 app.set('view engine', 'jade');
 app.use(express.static('public'));
@@ -8,18 +12,35 @@ app.get('/', function(req, res){
   res.redirect('/index');
 });
 
-app.get('/index', function (req, res) {
-  res.render('index', {})
+app.get('/index', function(req, res){
+  res.render('index', {});
 });
 
 app.get('/polls', function (req, res) {
-  res.render('polls', {})
+  mongo.connect(dbUrl, function(err, db) {
+    const polls = db.collection("polls");
+    polls.find({}, {question:1}).toArray(function(err, polls){
+      res.render('polls', {polls: polls});
+      db.close();
+    });
+  });
 });
 
-app.get('/poll', function (req, res) {
-  res.render('poll', {})
+app.get('/poll/:id([0-9a-fA-F]{24})', function (req, res) {
+  var pollObjectId = req.params.id;
+  mongo.connect(dbUrl, function(err, db) {
+      const polls = db.collection("polls");
+      polls.find({_id: ObjectID(pollObjectId)}, {_id:0, question:1, answers:1}).toArray(function(err, polls){
+        if (polls.length == 0) {
+          res.status(404).send("Could not find poll");
+        } else {
+          var poll = polls[0];
+          res.render('poll', {question: poll.question, answerString: JSON.stringify(poll.answers)});
+        }
+        db.close();
+      });
+    });
 });
-
 
 
 app.listen(process.env.port || 8080);
